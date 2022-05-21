5889164df891fcb6c97cb2bbc8ccd7382c770e8d
Serge Aleynikov <saleyn@gmail.com>
2022-05-21 11:21:32 -0400

    Update for WS with auth

--- src/coinbase_api_ws.erl
+++ src/coinbase_api_ws.erl
@@ -4,9 +4,12 @@
 
 -include_lib("kernel/include/logger.hrl").
 
--define(WS_URI, "wss://ws-feed.gdax.com").
+%% Sandbox API: https://docs.cloud.coinbase.com/exchange/docs/sandbox
 
--export([start_link/1]).
+-define(WS_URI,         "wss://ws-feed.exchange.coinbase.com").
+-define(WS_URI_SANDBOX, "wss://ws-feed-public.sandbox.exchange.coinbase.com").
+
+-export([start_link/1, reconnect/0]).
 
 -export([init/1,
          handle_call/3,
@@ -16,18 +19,44 @@
          code_change/3]).
 
 -record(state, {
-    callback :: pid(),
-    gun :: any()
+    callback  :: pid(),
+    uri       :: string(),
+    gun       :: any(),
+    products  :: [binary()],
+    channels  :: [binary()],
+    auth_key  :: binary(),
+    auth_pass :: binary(),
+    auth_secr :: binary()
 }).
 
 start_link(Callback) ->
     gen_server:start_link(?MODULE, [Callback], []).
 
+reconnect() ->
+    gen_server:cast(?MODULE, reconnect).
+
 init([Callback]) ->
-    State = #state{
-        callback=Callback
-    },
     gen_server:cast(self(), reconnect),
+    App            = application:get_application(),
+    URI            = application:get_env(App, uri,      ?WS_URI_SANDBOX),
+    Channels       = application:get_env(App, channels, [<<"full">>]),
+    {ok, Products} = application:get_env(App, products),
+    {ok, AuthFile} = application:get_env(App, auth_file),
+    {ok, AuthInfo} = file:read_file(AuthFile),
+    AuthData = re:split(AuthInfo, "([^= ]+)\s*=\s*(.+)\n", [trim, group]),
+    AuthList = [{K,V} || [_, K, V] <- AuthData],
+    #{"CB-ACCESS-KEY"        := AuthKey,
+      "CB-ACCESS-PASSPHRASE" := AuthPass,
+      "CB-ACCESS-SECRET"     := AuthSecret} = maps:from_list(AuthList),
+    State = #state{
+        callback  = Callback,
+        products  = Products,
+        channels  = Channels,
+        uri       = URI,
+        auth_key  = AuthKey,
+        auth_pass = AuthPass,
+        auth_secr = AuthSecret
+    },
     {ok, State}.
 
 handle_call(Request, _From, State) ->
@@ -62,7 +91,7 @@ handle_info({gun_down, Gun, _Proto, Reason, [], []}, State) ->
     gen_server:cast(self(), reconnect),
     {noreply, State};
 handle_info({gun_ws_upgrade, Gun, ok, _Headers}, State) ->
-    send_ws_message(Gun, subscribe_message()),
+    send_ws_message(Gun, subscribe_message(State)),
     {noreply, State};
 
 handle_info(Info, State) ->
@@ -79,24 +108,31 @@ code_change(OldVsn, State, _Extra) ->
 
 %% Internal methods
 
+reconnect_websocket(#state{gun=Pid} = State) when is_pid(Pid) ->
+    gun:close(Pid),
+    reconnect_websocket(State#state{gun=undefined});
 reconnect_websocket(State) ->
     {ok, #{scheme := "wss", host := Host, port := 443, path := Fragment}} =
-      url_string:parse(?WS_URI),
+      url_string:parse(State#state.uri),
     {ok, Pid} = gun:open(Host, 443, #{protocols => [http]}),
     _StreamRef = gun:ws_upgrade(Pid, Fragment),
     {ok, State#state{gun=Pid}}.
 
-subscribe_message() ->
-    [
-     {type, subscribe},
-     {product_ids, [
-        <<"BTC-USD">>, <<"LTC-USD">>, <<"ETH-USD">>
-     ]},
-     {channels, [
-        <<"heartbeat">>,
-        [{name, <<"ticker">>},
-         {product_ids, [<<"BTC-USD">>, <<"LTC-USD">>, <<"ETH-USD">>]}]
-     ]}
+subscribe_message(#state{products=Products, channels=Channels} = State) ->
+    Time   = integer_to_binary(erlang:system_time(second)),
+    Body   = <<"">>,
+    Data   = <<Time/binary, "GET", "/users/self/verify", Body/binary>>,
+    Hmac   = base64:decode(State#state.auth_secr),
+    SignB  = crypto:mac(hmac, sha256, Hmac, Data),
+    Sign   = base64:encode(SignB),
+    Body   = [
+     {type,       subscribe},
+     {product_ids,Products},
+     {channels,   [{name, C} || C <- Channels]},
+     {signature,  Sign},
+     {key,        State#state.auth_key},
+     {passphrase, State#state.auth_pass},
+     {timestamp,  Time}
     ].
 
 send_ws_message(Gun, Message) ->
@@ -142,5 +178,5 @@ parse_ws_message(<<"ticker">>, Message) ->
         best_ask=decimal:from_binary(proplists:get_value(<<"best_ask">>, Message))
     };
 parse_ws_message(Type, Message) ->
-    logger:info("Got unhandled message type '~s': ~p~n", [Type, Message]),
+    logger:info("CB msg ~s: ~p~n", [Type, Message]),
     undefined.
